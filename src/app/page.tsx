f8ddb66ed01578b7fa508f04b29b0fab58d20d99
prtmaars <prtmaars@gmail.com>
2025-09-07 15:36:46 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    // Deteksi bahasa browser
     const lang = navigator.language.startsWith("id") ? "id" : "en";
-
-    // Redirect ke /id atau /en
     router.replace(`/${lang}`);
   }, [router]);
 
